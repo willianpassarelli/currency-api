066b453fa084c3df29b8a3d6d9690b8da4c31e9b
willianpassarelli <willian.passarelli@hotmail.com>
2020-01-03 17:59:06 -0300

    redis config

--- src/app.js
+++ src/app.js
@@ -32,20 +32,20 @@ class App {
     this.server.use(cors());
     this.server.use(express.json());
 
-    // if (process.env.NODE_ENV !== 'development') {
-    //   this.server.use(
-    //     new RateLimit({
-    //       store: new RateLimitRedis({
-    //         client: redis.createClient({
-    //           host: process.env.REDIS_HOST,
-    //           port: process.env.REDIS_PORT,
-    //         }),
-    //       }),
-    //       windowMs: 1000 * 60 * 15,
-    //       max: 100,
-    //     })
-    //   );
-    // }
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
